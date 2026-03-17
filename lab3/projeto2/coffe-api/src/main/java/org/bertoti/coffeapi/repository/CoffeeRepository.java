94317086877a2aff8b7ae2fb450e99931186ec46
Giovanna Marques <giovanna.rodrigues9@fatec.sp.gov.br>
2026-03-17 18:13:08 -0300

    fix
    delete pasta projeto02

--- lab3/projeto2/coffe-api/src/main/java/org/bertoti/coffeapi/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.bertoti.coffeapi.repository;
-
-import org.bertoti.coffeapi.model.Coffee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoffeeRepository extends JpaRepository<Coffee, String> {
-}
