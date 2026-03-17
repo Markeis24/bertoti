79d31825e8ede3ad6be2086a1732757ef9cd4b04
Giovanna Marques <giovanna.rodrigues9@fatec.sp.gov.br>
2026-03-17 17:48:18 -0300

    feat
    implementação do postman

---
+++ lab3/projeto1/coffe-api/src/main/java/org/bertoti/coffeapi/CoffeApiApplication.java
@@ -0,0 +1,13 @@
+package org.bertoti.coffeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeApiApplication.class, args);
+    }
+
+}
